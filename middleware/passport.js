0427a13fbce39da67c8242918c47637de2388f56
vseguin <vincent.seguin@gobridgit.com>
2025-10-10 07:53:00 -0400

    fix(): user deserialize

--- middleware/passport.js
+++ middleware/passport.js
@@ -8,10 +8,13 @@ export const initializePassport = (passport, app) => {
     done(null, user.id)
   })
 
-  passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-      done(err, user)
-    })
+  passport.deserializeUser(async function (id, done) {
+    try {
+      const user = await User.findById(id)
+      done(null, user)
+    } catch (err) {
+      done(err)
+    }
   })
 
   passport.use(
